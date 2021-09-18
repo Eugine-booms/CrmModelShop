2255d7f772f3f4fa69e5187c8cd969c0ca5915ab
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-09-18 19:04:42 +0300

    промежуточные

--- CrmUi/AddFormBase.Designer.cs
+++ CrmUi/AddFormBase.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace CrmUi
 {
-    partial class CustomerOrSellerAddForm <T> where T: new ()
+    partial class AddFormBase
     {
         /// <summary>
         /// Required designer variable.
@@ -29,52 +29,51 @@ namespace CrmUi
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя";
             // 
             // textBox1
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(107, 12);
+            this.textBox1.Location = new System.Drawing.Point(47, 34);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(230, 20);
-            this.textBox1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Введите имя";
+            this.textBox1.Size = new System.Drawing.Size(290, 20);
+            this.textBox1.TabIndex = 1;
+            
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(280, 169);
+            this.button1.Location = new System.Drawing.Point(257, 155);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(105, 33);
             this.button1.TabIndex = 2;
             this.button1.Text = "Сохранить";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // CustomerAddForm
+            // AddFormBase
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(367, 204);
+            this.ClientSize = new System.Drawing.Size(384, 200);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Name = "CustomerAddForm";
-            this.Text = "CustomerForm";
-            this.Load += new System.EventHandler(this.CustomerForm_Load);
+            this.Controls.Add(this.label1);
+            this.Name = "AddFormBase";
+            this.Text = "AddFormBase";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,8 +81,8 @@ namespace CrmUi
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
     }
 }
---
+++ CrmUi/AddFormBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrmUi
+{
+    public partial class AddFormBase : Form
+    {
+        public AddFormBase()
+        {
+            InitializeComponent();
+        }
+                
+    }
+}
