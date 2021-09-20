5e4f7398b02632895f247eeb8d398a00ee6f5d4c
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-09-20 22:04:36 +0300

    For Generator add GetNewCashDesk GetNewCart+

--- CrmBl/Model/Generator.cs
+++ CrmBl/Model/Generator.cs
@@ -9,15 +9,41 @@ namespace CrmBl.Model
     public class Generator
     {
         Random rnd = new Random();
-        public List<Cart> Carts{ get; set;  }
-        public List<CashDesk> CashDesks { get; set; }
-        public List<Customer> Customers { get; set; }
-        public List<Check> Checks { get; set; }
+        //  public List<Cart> Carts{ get; set;  }
+        //  public List<Sell> Sells { get; set; }
+        //  public List<Check> Checks { get; set; }
+      //  public List<CashDesk> CashDesks { get; set; }
         public List<Product> Products { get; set; }
-        public List<Sell> Sells { get; set; }
+        public List<Customer> Customers { get; set; }
+       
         public List<Seller> Sellers { get; set; }
 
 
+        public List<CashDesk> GetNewCashDesk(int count, int vaxQueueLenght, List<Seller> sellers=null)
+        {
+
+            if (sellers==null)
+            {
+                sellers = GetNewSellers(count);
+            }
+            if (sellers.Count<count)
+            {
+                throw new ArgumentException(nameof(sellers.Count), "Мало продавцов");
+            }
+            var resultList = new List<CashDesk>();
+            for (int i = 0; i < count; i++)
+            {
+                var name = GetRndText();
+                var cashDesk = new CashDesk(i, sellers[i])
+                {
+                    Number = i,
+                    ExitCustomerCount = 0,
+                    MaxQueueLenght = vaxQueueLenght
+                };
+                resultList.Add(cashDesk);
+            }
+            return resultList;
+        }
         public List<Customer> GetNewCustomers(int count)
         {
             var resultList = new List<Customer>();
@@ -33,7 +59,7 @@ namespace CrmBl.Model
             }
             return resultList;
         }
-        public List<Seller> GetNewSells(int count)
+        public List<Seller> GetNewSellers(int count)
         {
             var resultList = new List<Seller>();
             for (int i = 0; i < count; i++)
@@ -48,6 +74,7 @@ namespace CrmBl.Model
             }
             return resultList;
         }
+      
         public List<Product> GetNewProducts(int count)
         {
             var resultList = new List<Product>();
@@ -62,6 +89,37 @@ namespace CrmBl.Model
                     Price = (decimal)(rnd.Next(5, 100000) + rnd.NextDouble())
                 };
                 resultList.Add(product);
+            }
+            return resultList;
+        }
+        public List<Cart> GetNewCart(int count, int productCount)
+        {
+            var resultList = new List<Cart>();
+            var customers = GetNewCustomers(count);
+            for (int i = 0; i < count; i++)
+            {
+                var cart = new Cart(customers[i]);
+                var products = GetNewProducts(productCount);
+                foreach (var product in products)
+                {
+                    cart.Add(product);
+                }
+                resultList.Add(cart);
+            }
+            return resultList;
+        }
+        public List<Cart> GetNewCart(int count, List<Product> products)
+        {
+            var resultList = new List<Cart>();
+            var customers = GetNewCustomers(count);
+            for (int i = 0; i < count; i++)
+            {
+                var cart = new Cart(customers[i]);
+                foreach (var product in products)
+                {
+                    cart.Add(product);
+                }
+                resultList.Add(cart);
             }
             return resultList;
         }
